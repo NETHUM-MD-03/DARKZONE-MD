26f1cc4b1c3662f52b8a28572c130c6738928128
NETHUM-MD-03 <nethumoshada458@gmail.com>
2025-11-16 01:41:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "IK~8M0RFS6K#a7pESPqbjlkeSzKPGUSy0muVpToWh73jf5tT_9PdTVo",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
